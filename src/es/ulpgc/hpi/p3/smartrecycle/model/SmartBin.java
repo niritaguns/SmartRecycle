e8e0f2b9895f410e8f6fc30c36b2dfb3bfb48673
adriistna <adriansanros05@gmail.com>
2025-12-09 18:48:24 +0000

    started smartbin code

--- src/es/ulpgc/hpi/p3/smartrecycle/model/SmartBin.java
+++ src/es/ulpgc/hpi/p3/smartrecycle/model/SmartBin.java
@@ -1,4 +1,20 @@
 package es.ulpgc.hpi.p3.smartrecycle.model;
 
 public class SmartBin {
+    private String binId;
+    private String locationGPS;
+    private int  maxCapacityLiters;
+    private int currentFillLevel;
+    private boolean isFull;
+
+
+    public SmartBin(String binId, String locationGPS, int maxCapacityLiters){
+        this.binId = binId;
+        this.locationGPS = locationGPS;
+        this.maxCapacityLiters = maxCapacityLiters;
+        this.currentFillLevel = 0;
+        this.isFull = false;
+    }
+
+
 }
