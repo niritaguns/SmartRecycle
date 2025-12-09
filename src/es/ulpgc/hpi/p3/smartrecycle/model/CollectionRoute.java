1b2fc38953a6d9783623341a3aac1318be48b95e
armas <armassniraa@gmail.com>
2025-12-09 18:49:13 +0000

    initial code collectionRoute

--- src/es/ulpgc/hpi/p3/smartrecycle/model/CollectionRoute.java
+++ src/es/ulpgc/hpi/p3/smartrecycle/model/CollectionRoute.java
@@ -1,4 +1,16 @@
 package es.ulpgc.hpi.p3.smartrecycle.model;
-
+import java.util.ArrayList;
+import java.util.List;
 public class CollectionRoute {
+    private String routeId;
+    private String truckId;
+    private List<SmartBin> binsToCollect;
+    private double estimatedDistanceKM;
+
+    public CollectionRoute(String routeId, String truckId) {
+        this.routeId = routeId;
+        this.truckId = truckId;
+        this.binsToCollect = new ArrayList<>();
+        this.estimatedDistanceKM = 0.0;
+    }
 }
