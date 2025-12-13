2a0ff5911279d741f1543a897e85a9df6a045727
armas <armassniraa@gmail.com>
2025-12-13 13:46:24 +0000

    final code collectionRoute

--- src/es/ulpgc/hpi/p3/smartrecycle/model/CollectionRoute.java
+++ src/es/ulpgc/hpi/p3/smartrecycle/model/CollectionRoute.java
@@ -13,4 +13,32 @@ public class CollectionRoute {
         this.binsToCollect = new ArrayList<>();
         this.estimatedDistanceKM = 0.0;
     }
+
+    public void addBinToRoute(SmartBin bin) {
+        this.binsToCollect.add(bin);
+        recalculateDistance();
+        System.out.println("-> Contenedor " + bin.getBinId() + " añadido a la Ruta " + routeId);
+    }
+
+    private void recalculateDistance() {
+        this.estimatedDistanceKM = this.binsToCollect.size() * 2.5;
+    }
+
+    public void generateRouteSummary() {
+        System.out.println("\n--- Resumen de la Ruta: " + routeId + " ---");
+        System.out.println("Camión Asignado: " + truckId);
+        System.out.println("Contenedores a Recoger: " + binsToCollect.size());
+        System.out.println("Distancia Total Estimada: " + estimatedDistanceKM + " KM");
+
+        System.out.print("Lista de Bins: ");
+        for (SmartBin bin : binsToCollect) {
+            System.out.print(bin.getBinId() + " (" + bin.getCurrentFillLevel() + "%); ");
+        }
+        System.out.println();
+    }
+
+    public double getEstimatedDistanceKM() {
+        return estimatedDistanceKM;
+    }
+}
 }
